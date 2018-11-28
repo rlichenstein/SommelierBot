3560bac8217eb686def86061de1e285c90a4ecc1
Campbell M. Boswell <31942540+cmboswell@users.noreply.github.com>
2018-11-27 20:55:50 -0500

    Add files via upload

--- process_review.py
+++ process_review.py
@@ -17,28 +17,31 @@ def review_tf_idf(keyword_dict, review_list):
         d. append the feature list to a master list of all examples
 """
 import csv
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob
 import math
+from nltk.probability import FreqDist
 
 
 def import_reviews():
-    print('importing reviews from csv')
+    print('importing reviews from csv [', end='',flush=True)
     review_list = []        #a list of all the reviews we process
     stop_words = set(stopwords.words('english'))
 
-    with open('winemag-data-130k-v2.csv', mode='r', encoding='utf8') as csv_file:
-        print('opened the csv')
+    with open('winemag-data-130k.csv', mode='r', encoding='utf8') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         row = next(csv_reader) #skip the first row which contains lables
         i = 0
         for row in csv_reader:
 
-            if i > 1000:
-                break
+            #for efficient debugging
+            #if i > 1000:
+            #    break
 
-            print('reading document #' + str(i) +' out of 100')
+            if (i % 1000) == 0:
+                print('*', end='',flush=True)
             review_as_list = []     #a single review in list form
             review_as_text = row[2]
 
@@ -47,89 +50,88 @@ def import_reviews():
             word_tokens = word_tokenize(review_as_text)
             for w in word_tokens:
                 if w not in stop_words:
-                    review_as_list.append(w.lower())
+                    if len(w) > 3:
+                        review_as_list.append(w.lower())
 
             review_list.append(' '.join(review_as_list))
             i += 1
 
-        print('finished reading csv')
+        print(']')
 
     return review_list
 
 
-def build_word_dict(review_list):
-    print('building word dictionary')
-    keyword_dict = {} #dictionary to store all keywords across all docs
-    i = 0 #value to calculate coresponding indexes for feature list we are going to generate - these will be store in the keyword_dict
+def build_word_dict(review_list, frequency):
+    print('building word dictionary [', end='', flush=True)
+    keyword_dict = {} # a dictionary of keywords and indices to be returned
 
-    for review in review_list:
-        word_tokens = word_tokenize(review)
-        for w in word_tokens:
-            if w not in keyword_dict:
-                keyword_dict.update({ w : i})
-                i += 1
+    #concatenate all the reviews to calculate the frequency of terms in all reviews
+    review_blob = ''.join(review_list)
+    words = word_tokenize(review_blob) #tokenize words in review blob
+    freq_dist = FreqDist(words) #get the frequency distributions of all words
+    #print(freq_dist)
+    keywords = freq_dist.most_common(frequency) # create a list of the most common words
 
-    print(str(keyword_dict))
+    i = 0
+    for word in keywords:
+        #print progess
+        if (i % 10) == 0:
+            print('*', end='', flush=True)
+
+        keyword_dict.update({ word[0] : i})
+        i += 1
+
+    print(']')
     return keyword_dict
 
 
-def review_tf_idf(keyword_dict, review_list):
-    print('generating tf-idf scores')
+def assign_features(keyword_dict, review_list):
+    print('generating frequency scores [', end='', flush=True)
     feature_count = len(keyword_dict)
     feature_list = []
     bloblist = []
 
-    #convert reviews in to textblobs and store them in a list of textblobs
+    i = 0
+    #itterate through reviews and mark instanes of feature words
     for review in review_list:
-        bloblist.append(TextBlob(review))
+        #print progess
+        if (i % 1000) == 0:
+            print('*', end='', flush=True)
 
-    #itterate through blobs and compute tf-idf scores for each word
-    for blob in bloblist:
+        #create our feature vector
         features = [0] * feature_count
-        for word in blob.words:
-            score = tf_idf(word, blob, bloblist)
-            index = keyword_dict.get(word)
-            if index == None:
 
-                print("ERROR, " + word + " NOT IN DICTIONARY")
-                continue
-            features[index] = score
+        word_tokens = word_tokenize(review)
+        for w in word_tokens:
+            if w in keyword_dict:
+                index = keyword_dict.get(w)
+
+            #insert the score at the proper coresponding index
+            features[index] +=1
+
         feature_list.append(features)
+        i+=1
 
+    print(']')
     return feature_list
 
-def tf_idf(word, blob, bloblist):
-    return tf(word, blob) * idf(word, bloblist)
 
-def tf(word, blob):
-    return blob.words.count(word) / len(blob.words)
+def write_features(feature_list):
+    '''Write the features we generated across all data to a csv so that is can
+       be easily fed to our learning algorithm'''
+    print("writing fetures to csv")
+    with open('review_features.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for features in feature_list:
+            writer.writerow(features)
 
-def n_containing(word, bloblist):
-    return sum(1 for blob in bloblist if word in blob)
-
-def idf(word, bloblist):
-    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
 
 
 def main():
     review_list = import_reviews()
-    keyword_dict = build_word_dict(review_list)
-    feature_list = review_tf_idf(keyword_dict, review_list)
-    print(str(feature_list[0]))
-
-    feature_set = set()
-    num_empty_docs = 0
-    for features in feature_list:
-        doc_feature_count = 0
-        for i in range(len(features)):
-            if features[i] >= 0.1:
-                feature_set.add(i)
-                doc_feature_count += 1
-        if doc_feature_count == 0:
-            num_empty_docs += 1
-
-    print("feature_count: " + str(len(feature_set)))
-    print("num_empty_docs: " +str(num_empty_docs))
+    keyword_dict = build_word_dict(review_list, 1000)
+    feature_list = assign_features(keyword_dict, review_list)
+    write_features(feature_list)
 
 
 
