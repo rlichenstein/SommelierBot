2d95ab91159205d4c0a5f8728efefcbdff5deb8e
Campbell M. Boswell <31942540+cmboswell@users.noreply.github.com>
2018-11-30 13:41:27 -0500

    Add files via upload
    Updated process_review.py file which filters out non-descriptive keywords that are listed in keyword_filter.py. process_review.py outputs a csv of content vectors with 200 features per vector. A copy of this csv is in the file review_features.zip.

--- process_review.py
+++ process_review.py
@@ -23,7 +23,7 @@ from nltk.tokenize import word_tokenize
 from textblob import TextBlob
 import math
 from nltk.probability import FreqDist
-
+import keyword_filter
 
 def import_reviews():
     print('importing reviews from csv [', end='',flush=True)
@@ -63,17 +63,37 @@ def import_reviews():
 
 def build_word_dict(review_list, frequency):
     print('building word dictionary [', end='', flush=True)
-    keyword_dict = {} # a dictionary of keywords and indices to be returned
+
+    keyword_dict = {} #a dictionary of keywords and indices to be returned
+    filter_list = keyword_filter.filter_list #a manually formed list of words to filter out
 
     #concatenate all the reviews to calculate the frequency of terms in all reviews
     review_blob = ''.join(review_list)
     words = word_tokenize(review_blob) #tokenize words in review blob
     freq_dist = FreqDist(words) #get the frequency distributions of all words
-    #print(freq_dist)
     keywords = freq_dist.most_common(frequency) # create a list of the most common words
+
+    print("keywords length: " + str(len(keywords)))
+    keyword_count = 0
+    i = 0
+
+    while i < len(keywords):
+
+        if keywords[i][0] in filter_list:
+            del keywords[i]
+        else:
+            i += 1
+        keyword_count += 1
+
+    #Last == ('great', 4820)
+    print(keywords)
+    print("Initial dict length: " + str(frequency) + " new dict length: " + str(i))
+
 
     i = 0
     for word in keywords:
+        if i > 272:
+            print(word)
         #print progess
         if (i % 10) == 0:
             print('*', end='', flush=True)
@@ -106,8 +126,8 @@ def assign_features(keyword_dict, review_list):
             if w in keyword_dict:
                 index = keyword_dict.get(w)
 
-            #insert the score at the proper coresponding index
-            features[index] +=1
+                #insert the score at the proper coresponding index
+                features[index] +=1
 
         feature_list.append(features)
         i+=1
@@ -116,12 +136,19 @@ def assign_features(keyword_dict, review_list):
     return feature_list
 
 
-def write_features(feature_list):
+def write_features(feature_list, keyword_dict):
     '''Write the features we generated across all data to a csv so that is can
        be easily fed to our learning algorithm'''
     print("writing fetures to csv")
     with open('review_features.csv', 'w', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
+
+        #write label row for keywords
+        keyword_list = []
+        for keyword in keyword_dict:
+            keyword_list.append(keyword)
+        writer.writerow(keyword_list)
+
         for features in feature_list:
             writer.writerow(features)
 
@@ -129,9 +156,26 @@ def write_features(feature_list):
 
 def main():
     review_list = import_reviews()
-    keyword_dict = build_word_dict(review_list, 1000)
+    keyword_dict = build_word_dict(review_list, 285)
     feature_list = assign_features(keyword_dict, review_list)
-    write_features(feature_list)
+    write_features(feature_list, keyword_dict)
+
+    #Debugging output to ensure that we hitting features in all documents
+    print("We have " + str(len(feature_list[0])) + " features...")
+
+    feature_set = set()
+    num_empty_docs = 0
+    for features in feature_list:
+        doc_feature_count = 0
+        for i in range(len(features)):
+            if features[i] != 0:
+                feature_set.add(i)
+                doc_feature_count += 1
+        if doc_feature_count == 0:
+            num_empty_docs += 1
+
+    print("feature_count: " + str(len(feature_set)))
+    print("num_empty_docs: " +str(num_empty_docs))
 
 
 
